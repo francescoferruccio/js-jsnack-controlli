73e57c073175f37756f7952a729edc4cb0ff7455
Francesco <francesco.ferruccio88@gmail.com>
2020-03-20 14:42:02 +0100

    ex-2 bonus: controllo su tipo dati secondo stessa logica di ex-1

---
+++ ex-2/bonus/script.js
@@ -0,0 +1,22 @@
+// L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+var msg;
+
+// chiediamo all'utente di inserire due parole e le salviamo
+var parola1 = prompt("Inserisci la prima parola");
+var parola2 = prompt("Inserisci la seconda parola");
+
+// confrontiamo la lunghezza delle due parole e gestiamo l'ordine delle parole in output di conseguenza
+if (isNaN(parola1) && isNaN(parola2)) {
+  if (parola1.length < parola2.length) {
+    msg = "Parola più corta: " + parola1 + " Parola più lunga: " + parola2;
+  } else if (parola1.length > parola2.length) {
+    msg = "Parola più corta: " + parola2 + " Parola più lunga: " + parola1;
+  } else {
+    msg = parola1 + " e " + parola2 + " sono lunghe uguali."
+  }
+} else {
+  msg = "Una delle due parole inserite non è una stringa."
+}
+
+// stampiamo il risultato a schermo
+document.getElementById('testo').innerHTML = msg;
